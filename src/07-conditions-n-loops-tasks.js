c199d4d2c57e1ad4fda03afd5063c8c946600304
nikolai-chernolutskii <nikolai@site-now.online>
2023-01-19 22:24:24 +0200

    feat: conditions and loops, part 3

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -290,8 +290,20 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const str = ccn.toString();
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    let cardNum = +str[i];
+    if ((str.length - i) % 2 === 0) {
+      cardNum *= 2;
+      if (cardNum > 9) {
+        cardNum -= 9;
+      }
+    }
+    sum += cardNum;
+  }
+  return sum % 10 === 0;
 }
 
 /**
